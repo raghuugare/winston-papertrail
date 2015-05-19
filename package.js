637c41999b257ced995c16a72330f9b27d3820ee
Raghavendra Ugare <rags@ragsmba.local>
2015-05-19 16:50:08 +0530

    Updated the version 0.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rugare:winston-papertrail',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'A meteor wrapper over the winston-papertrail transport',
   // URL to the Git repository containing the source code for this package.
